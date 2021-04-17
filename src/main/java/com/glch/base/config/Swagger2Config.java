a65b5034a50553ade76d084c6c8696715b105bfa
hczzl <362349050@qq.com>
2021-04-17 15:29:43 +0800

    去掉redis认证

--- src/main/java/com/glch/base/config/Swagger2Config.java
+++ src/main/java/com/glch/base/config/Swagger2Config.java
@@ -1,22 +1,15 @@
 package com.glch.base.config;
 
 import io.swagger.annotations.ApiOperation;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import sun.security.util.SecurityConstants;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
